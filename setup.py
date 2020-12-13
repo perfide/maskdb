e21b34ff92e267a41528b10851f44c0f269790c2
perfide <github.perfide@safersignup.com>
2020-12-14 00:00:00 +0100

    chore: add config for setuptools

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright:
+#   2020 P. H. <github.com/perfide>
+# License:
+#   AGPL-3.0-only (GNU Affero General Public License v3.0 only)
+#   https://spdx.org/licenses/AGPL-3.0-only.html
+
+"""Stub-script required by setuptools"""
+
+import setuptools
+
+setuptools.setup()
+
+# [EOF]
